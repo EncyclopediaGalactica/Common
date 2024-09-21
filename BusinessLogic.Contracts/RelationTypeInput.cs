94fa978598f74e476f3491949bd40d100f6291bc
Andras Csanyi <andras.csanyi@pm.me>
2024-09-21 20:01:30 +0200

    impl

---
+++ BusinessLogic.Contracts/RelationTypeInput.cs
@@ -0,0 +1,8 @@
+namespace EncyclopediaGalactica.BusinessLogic.Contracts;
+
+public class RelationTypeInput
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
